7ef5c167b9a2178ab4be44f90e7b1bf6c9855b24
mvinorian <vinorianmuhammad@gmail.com>
2023-05-20 23:51:19 +0700

    feat: initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# How To Run
+
+1. Install the dependencies with `npm ci`
+2. Run into localhost with `npm run start`
+3. Open browser with the url in terminal, usually [localhost:5173](http://localhost:5173/)
---
+++ pages/extrude-geometry/main.js
@@ -0,0 +1,33 @@
+import * as THREE from "three";
+
+const container = document.getElementById("three");
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(
+  75,
+  container.clientWidth / container.clientHeight,
+  0.1,
+  1000
+);
+
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+
+camera.position.setZ(5);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(container.clientWidth, container.clientHeight);
+container.appendChild(renderer.domElement);
+
+function animate() {
+  requestAnimationFrame(animate);
+
+  cube.rotateX(0.01);
+  cube.rotateY(0.005);
+  cube.rotateZ(0.015);
+
+  renderer.render(scene, camera);
+}
+
+animate();

4702ca8f079aa45ff3221b07c7c9a3d67a90846e
frederick <frederickhidayat2003@gmail.com>
2023-05-22 14:26:18 +0700

    feat: extrude and tube geometry

--- pages/tube-geometry/main.js
+++ pages/tube-geometry/main.js
@@ -9,12 +9,30 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+class CustomSinCurve extends THREE.Curve {
 
-camera.position.setZ(5);
+	constructor( scale = 1 ) {
+		super();
+		this.scale = scale;
+	}
+
+	getPoint( t, optionalTarget = new THREE.Vector3() ) {
+
+		const tx = t * 3 - 1.5;
+		const ty = Math.sin( 2 * Math.PI * t );
+		const tz = 0;
+
+		return optionalTarget.set( tx, ty, tz ).multiplyScalar( this.scale );
+	}
+}
+
+const path = new CustomSinCurve( 10 );
+const geometry = new THREE.TubeGeometry( path, 20, 2, 8, false );
+const material = new THREE.MeshBasicMaterial( { color: 0x90e0ef, wireframe: true } );
+const tube = new THREE.Mesh( geometry, material );
+scene.add( tube );
+
+camera.position.setZ(50);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(container.clientWidth, container.clientHeight);
@@ -23,9 +41,9 @@ container.appendChild(renderer.domElement);
 function animate() {
   requestAnimationFrame(animate);
 
-  cube.rotateX(0.01);
-  cube.rotateY(0.005);
-  cube.rotateZ(0.015);
+  tube.rotateX(0.01);
+  tube.rotateY(0.005);
+  tube.rotateZ(0.015);
 
   renderer.render(scene, camera);
 }

50d89b880b203bd1c1ea6256e0d2f587f4ca45f2
kalyanaalk <kanza.putri@yahoo.com>
2023-05-22 12:50:33 +0700

    add binary operation

--- pages/binary-operation/main.js
+++ pages/binary-operation/main.js
@@ -1,6 +1,7 @@
-import * as THREE from "three";
+import * as THREE from 'three';
+import { ThreeBSP } from 'three-js-csg';
 
-const container = document.getElementById("three");
+const container = document.getElementById('three');
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -9,10 +10,15 @@ const camera = new THREE.PerspectiveCamera(
   1000
 );
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+const geometryA = new THREE.BoxGeometry(1, 1, 1);
+const materialA = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const meshA = new THREE.Mesh(geometryA, materialA);
+scene.add(meshA);
+
+const geometryB = new THREE.SphereGeometry(0.5, 32, 32);
+const materialB = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const meshB = new THREE.Mesh(geometryB, materialB);
+scene.add(meshB);
 
 camera.position.setZ(5);
 
@@ -23,11 +29,29 @@ container.appendChild(renderer.domElement);
 function animate() {
   requestAnimationFrame(animate);
 
-  cube.rotateX(0.01);
-  cube.rotateY(0.005);
-  cube.rotateZ(0.015);
+  // Perform the binary operation (subtract)
+  const resultGeometry = performBinaryOperation(geometryA, geometryB);
+
+  // Create a mesh from the result geometry
+  const resultMaterial = new THREE.MeshBasicMaterial({ color: 0x0000ff });
+  const resultMesh = new THREE.Mesh(resultGeometry, resultMaterial);
+  scene.add(resultMesh);
 
   renderer.render(scene, camera);
+}
+
+function performBinaryOperation(geometryA, geometryB) {
+  // Convert geometries to ThreeBSP objects
+  const bspA = new ThreeBSP(geometryA);
+  const bspB = new ThreeBSP(geometryB);
+
+  // Perform the subtraction operation
+  const resultBSP = bspA.substract(bspB);
+
+  // Convert the result back to a Three.js geometry
+  const resultGeometry = resultBSP.toMesh();
+
+  return resultGeometry;
 }
 
 animate();
